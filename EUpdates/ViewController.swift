a4200d4f2f24c1e525bde42a5ebb4f36c96e9c7e
xartemx <adtest78@gmail.com>
2014-11-17 12:53:39 -0600

    Initial Commit

---
+++ EUpdates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EUpdates
+//
+//  Created by Artsiom Dolia on 11/17/14.
+//  Copyright (c) 2014 Artsiom Dolia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
